e526189837cd1662c2e140df6540b21a997c96f5
Greg Doud <gpdoud@gmail.com>
2022-09-26 20:25:20 -0400

    updates for Leslie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'multi-select-drop-down';
+  // this represents the Item table
+  items: any[] = [
+    { id: 1, description: "Item 1" },
+    { id: 2, description: "Item 2" },
+    { id: 3, description: "Item 3" },
+    { id: 4, description: "Item 4" }
+  ]
+  // This holds the values from the option tags
+  // that have been selected on the page
+  selections: any[] = [];
+  // this holds each item selected
+  quotelines: any[] = [];
+  // This simulates reading the database
+  // to get an Item by Primary Key and 
+  // returning an instance of it.
+  GetItemByPk(id: number) {
+    for (let i of this.items) {
+      if (id == i.id) {
+        return i;
+      }
+    }
+  }
+  // This function reads all the ids selected
+  // then reads the Item table for that id
+  // and stored each item read in the quotelines
+  // collection
+  display(): void {
+    console.log(this.selections);
+    for (let id of this.selections) {
+      let quoteLine = this.GetItemByPk(id);
+      this.quotelines.push(quoteLine);
+    }
+    console.table(this.quotelines);
+  }
 }
